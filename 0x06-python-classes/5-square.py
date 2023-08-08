66ccdee8715eb3362a8f96acd08c7ad2ced21c10
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-08 14:59:55 +0100

    print square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,6 +32,8 @@ class Square():
     def my_print(self):
         if self.__size == 0:
             print()
+            return
+
         for i in range(self.__size):
             for i in range(self.__size):
                 print('#',end='')
