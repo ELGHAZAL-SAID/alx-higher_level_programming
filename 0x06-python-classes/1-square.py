a0b2e694b2be30c6dd8d1669e13bd8783ae35a35
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-08 13:47:16 +0100

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""square class to represent a square"""
+
+
+class Square():
+    """Empty Square class"""
+    def __init__(self, size = None):
+        
+        """constructor
+            args:
+                size (no type) : size of the square
+        """
+        
+        self.__size = size
