0a5ca115ad11dc31bf676da83098dce24dd0f414
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-18 00:23:42 +0100

    foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,8 @@ USE hbtn_0d_usa
 -- create table states if not exists
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    state_id INT NOT NULL,FOREIGN KEY (state_id) REFERENCES states(id)
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id)
 )
