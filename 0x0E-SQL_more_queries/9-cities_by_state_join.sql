cde8425176655755e57c1019a106bf4861a0f2f0
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-18 00:38:38 +0100

    join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, citeies.name, states.name
 FROM cities c, states s
-where s.state_id = c.id; 
+where c.state_id = s.id; 
