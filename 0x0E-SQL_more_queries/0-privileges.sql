16626a227491dfe012c138fdf5a912a0d8d78c78
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-17 22:49:52 +0100

    show grants

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- show all privileges of the mysql users
 -- Query to list all privileges of the mysql users
-SHOW IF EXISTS GRANTS FOR 'user_0d_1'@'localhost';
-SHOW IF EXISTS GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- create user with all privilege
 CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+WH
