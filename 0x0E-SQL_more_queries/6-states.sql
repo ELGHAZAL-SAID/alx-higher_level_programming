65c09fba826f5159727c9e39af311c8e959c4154
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-18 00:14:47 +0100

    peimary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,9 @@
 -- create databese hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
+USE hbtn_0d_usa
 -- create table states if not exists
 CREATE TABLE IF NOT EXISTS states(
-    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(256) NOT NULL
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id)
 )
