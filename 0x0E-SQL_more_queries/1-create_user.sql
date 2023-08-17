c6fa37e9d18252e014b2a8c2636211193df44842
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-17 22:55:36 +0100

    grant prv

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create user with all privilege
-CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGE ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
