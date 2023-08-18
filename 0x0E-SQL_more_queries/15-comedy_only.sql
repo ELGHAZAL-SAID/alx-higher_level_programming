8cf4296146007142424652b052ebb0efd2f16541
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-18 02:04:46 +0100

    join

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 --  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
 SELECT tv_shows.title
-FROM tv_shows JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
+FROM tv_shows INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.show_id
 WHERE tv_genres.name = 'Comedy'
