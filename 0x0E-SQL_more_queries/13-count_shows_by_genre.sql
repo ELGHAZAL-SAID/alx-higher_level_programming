a00de257df7e7839b2b8ba9ffb327f30612e9379
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-18 01:30:39 +0100

    join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
-SELECT tv_genres.name AS geners, count(tv_show_genres.show_id) AS number_of_shows
+--  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT tv_genres.name AS genre, count(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres, tv_show_genres
 WHERE tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
