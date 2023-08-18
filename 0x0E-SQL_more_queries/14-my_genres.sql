5f72641ea64a8e9c85e7b0cb1d3887da2c881691
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-18 01:52:44 +0100

    join

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,4 @@ ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name DESC;
+ORDER BY tv_genres.name;
