d71679ed3100b54638bbd7d7a89b7f0a2242c343
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-18 02:14:25 +0100

    last

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -5,7 +5,7 @@ SELECT tv_genres.name
 FROM tv_genres
 LEFT JOIN
 (
-	SELECT tv_genres.id, tv_genres.name -- Query to get Dexter genres
+	SELECT tv_genres.id, tv_genres.name
 	FROM tv_genres
 	JOIN tv_show_genres
 		ON tv_genres.id = tv_show_genres.genre_id
